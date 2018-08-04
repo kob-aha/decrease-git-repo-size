89ae99b00b4b92cd5e6092e8454fcb7de018e653
Koby Aharon <kobyahron@gmail.com>
2018-08-04 20:08:31 +0300

    Output file size in MB

--- run-analysis.sh
+++ run-analysis.sh
@@ -10,5 +10,6 @@ fi
 cd $FOLDER
 
 join -o "1.1 1.2 2.3" <(git rev-list --objects --all | sort) <(git verify-pack -v objects/pack/*.idx | sort -k3 -n | tail -5 | sort)  \
-	| sort -k3 -n -r
+	| sort -k3 -n -r \
+	| awk '{ printf ("%s %s %sMB\n", $1, $2, $3/1024/1024) }'
 
